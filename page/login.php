601cbba83693fbd21070c0685aab16a9afb27fe3
Kosevskey <108695178+Kosevskey@users.noreply.github.com>
2025-02-11 09:54:47 +0100

    Add files via upload

---
+++ page/login.php
@@ -0,0 +1,11 @@
+<div class="mb-3">
+  <label for="exampleFormControlInput1" class="form-label">Email</label>
+  <input type="email" class="form-control" id="exampleFormControlInput1" placeholder="name@example.com">
+</div>
+<div class="mb-3">
+  <label for="exampleFormControlInput2" class="form-label">Passwort</label>
+  <input type="password" class="form-control" id="exampleFormControlInput2">
+</div>
+<div class="col-auto">
+    <button type="submit" class="btn btn-primary mb-3">Einloggen</button>
+  </div>
