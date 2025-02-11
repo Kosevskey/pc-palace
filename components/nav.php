06f228f466800f82d8a56ad9a13c17976bb76b4a
Kosevskey <108695178+Kosevskey@users.noreply.github.com>
2025-02-11 10:54:58 +0100

    Add files via upload

--- components/nav.php
+++ components/nav.php
@@ -17,10 +17,10 @@
             Dropdown
           </a>
           <ul class="dropdown-menu">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
+            <li><a class="dropdown-item" href="?page=login">Login</a></li>
+            <li><a class="dropdown-item" href="#">Registrieren</a></li>
             <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
+            <li><a class="dropdown-item" href="?page=admin">Admin</a></li>
           </ul>
         </li>
     </div>
