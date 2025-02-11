9da7c9e672a31a3158c2f61f209d47946538387d
Kosevskey <108695178+Kosevskey@users.noreply.github.com>
2025-02-11 10:37:57 +0100

    Add files via upload

---
+++ page/admin.php
@@ -0,0 +1,77 @@
+<h1 class="heading">Adminbereich</h1>
+<h2>Neues Produkt</h2>
+
+<div class="add-product">
+    <div class="mb-3">
+        <label for="pc-name" class="form-label">PC Name</label>
+        <input type="text" class="form-control" id="pc-name">
+    </div>
+
+
+    <div class="mb-3">
+        <label for="dateien" class="form-label">Dateien auswählen (drei Bilder)</label>
+        <input type="file" multiple="3" class="form-control" id="dateien">
+    </div>
+
+    <div class="mb-3">
+        <label for="beschreibung" class="form-label">Example textarea</label>
+        <textarea class="form-control" id="beschreibung" rows="2"></textarea>
+    </div>
+
+    <div class="mb-3">
+        <label for="preis" class="form-label">Preis</label>
+        <input type="text" class="form-control" id="preis">
+    </div>
+</div>
+
+<h2>Bauteile</h2>
+<div class="add-bauteil">
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+    <div class="form-bauteil">
+        <label class="form-label">PC-Teil</label>
+        <input type="text" class="form-control">
+        <label class="form-label">Details</label>
+        <input type="text" class="form-control">
+    </div>
+</div>
